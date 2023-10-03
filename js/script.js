e71767dc2fd6485c812364a6479b1645d56629ff
RizkiFajar693 <134209533+RizkiFajar693@users.noreply.github.com>
2023-10-03 22:24:55 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -10,29 +10,3 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 }
 
-function login() {
-    // let username = document.getElementById('username').value;
-    // let password = document.getElementById('password').value;
-    // Add your login logic here
-    //  console.log('Username: ' + username);
-    //  console.log('Password: ' + password);
-    let isLogin = document.querySelector('button');
-    if (isLogin=true) {
-        
-    }
-}
-
-  function signup() {
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('Email').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('Confirm Password').value;
-    // Add your login logic here
-    console.log('Username: ' + username);
-    console.log('Password: ' + password);
-}
-function Forgot(){
-    document.getElementById('forgotPasswordButton').addEventListener('click', function() {
-        window.location.href = 'signup.html';
-    });
-}
